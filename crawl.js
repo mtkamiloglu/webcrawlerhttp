96f1544413c5a6d25a9a3acd5d933073efae2925
Muhammet Tayyip Kamiloğlu <mtk@Muhammets-MacBook-Air-3.local>
2023-02-18 21:12:36 +0300

    getURLsFromHTML funct and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,34 @@
+const { JSDOM } = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window._document.querySelectorAll('a')
+    
+    for(const linkElement of linkElements){
+        if(linkElement.href.slice(0,1) === '/'){
+            //relative URL
+            try{
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch(err) {
+                console.log(`error with relaitve url: ${err.message}`)
+            }
+        }
+        else{
+            // absolute URL
+            try{
+                const urlObj = new URL(linkElement.href)
+            urls.push(urlObj.href)    
+            } catch (err) { 
+                console.log(`error with absolute path: ${err.message}`)
+            }
+            
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString){
     const urlObject = new URL(urlString)
     const hostpath = `${urlObject.hostname}${urlObject.pathname}`
@@ -9,5 +40,6 @@ function normalizeURL(urlString){
 
 //makes the function inside of it available to other js files
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
