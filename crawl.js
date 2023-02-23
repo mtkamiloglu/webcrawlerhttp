1afb18c8ecc94e94e13fcf624a305929517e2776
Muhammet Tayyip Kamiloğlu <mtk@Muhammets-MacBook-Air-3.local>
2023-02-23 22:12:00 +0300

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,27 +1,47 @@
 const { JSDOM } = require('jsdom')
 
-async function crawlPage(currentURL){
-    console.log(`actively crawling: ${currentURL}`)
+async function crawlPage(baseURL, currentURL, pages){
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if(currentURLObj.hostname !== baseURLObj.hostname){
+        return pages
+    }
 
+    const normalizedURL = normalizeURL(currentURL)
+    if(pages[normalizedURL] > 0){
+        pages[normalizedURL]++
+        return pages
+    }
+
+    pages[normalizedURL] = 1
+
+    console.log(`actively crawling: ${currentURL}`)
+    let htmlBody = ``
     try{
         const resp = await fetch(currentURL)
 
         if(resp.status > 399){
             console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL }`)
-            return
+            return pages
         }
 
         const contentType = resp.headers.get("content-type")
         if(!contentType.includes("text/html")){
             console.log(`non html response, content type: ${contentType}, on page: ${currentURL }`)
-            return
+            return pages
         }
 
-        console.log(await resp.text())
+        htmlBody = await resp.text()
     } catch(err){
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
-    
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+    for (const nextURL of nextURLs){
+        pages = await crawlPage(baseURL, nextURL, pages)
+    }
+
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL){
@@ -36,7 +56,7 @@ function getURLsFromHTML(htmlBody, baseURL){
                 const urlObj = new URL(`${baseURL}${linkElement.href}`)
                 urls.push(urlObj.href)
             } catch(err) {
-                console.log(`error with relaitve url: ${err.message}`)
+                console.log(`error with relative url: ${err.message}`)
             }
         }
         else{
@@ -55,11 +75,11 @@ function getURLsFromHTML(htmlBody, baseURL){
 
 function normalizeURL(urlString){
     const urlObject = new URL(urlString)
-    const hostpath = `${urlObject.hostname}${urlObject.pathname}`
-    if(hostpath.length > 0 && hostpath.slice(-1) === '/'){
-        return hostpath.slice(0, -1)
+    let fullPath = `${urlObject.host}${urlObject.pathname}`
+    if(fullPath.length > 0 && fullPath.slice(-1) === '/'){
+        return fullPath.slice(0, -1)
     }
-    return hostpath
+    return fullPath
 }
 
 //makes the function inside of it available to other js files
--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const {crawlPage} = require("./crawl.js")
 
 
-function main(){
+async function main(){
     if(process.argv.length < 3){
         console.log("no website provided")
-        process.exit(1)
     }
     if(process.argv.length > 3){
         console.log("too many command line args")
-        process.exit(1)
     }
 
     const baseURL = process.argv[2]
-    console.log(`starting crawl of ${baseURL}`)
+    console.log(`starting crawl of ${baseURL}...`)
 
-    crawlPage(baseURL)
+    const pages = await crawlPage(baseURL, baseURL, {})
+
+    console.log(pages)
 }
 
 main()
